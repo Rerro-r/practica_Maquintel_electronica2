77ffd4639fb953ad3b6a6fc5d9271b18f8eee8f7
Renate Rehbein <renate.rehbein@uc.cl>
2025-01-17 15:37:17 -0300

    App robusta a errores

--- Odómetro/Plan B/Envío binario/Codigo Odometria/odometria 4.25.py
+++ Odómetro/Plan B/Envío binario/Codigo Odometria/odometria 4.25.py
@@ -96,21 +96,19 @@ def procesar_imu(imu_data): #funcion para procesar los datos del imu
 
 
 def desconectar():
-    global Estado, envio_encoder, puertoSerial_c, data_thread#, Ticks, ti_ant
+    global Estado, envio_encoder, puertoSerial_c, data_thread
     Estado = 0
     envio_encoder = True
-   # Ticks = 0
-   # ti_ant = 0
     habilitar_botones(True, False, False)
     mensaje = "STOP"
-    print(mensaje)
     if data_thread and data_thread.is_alive(): #esperar a que el hilo termine
         Estado = 0
         data_thread.join(timeout=0.1) # esperar que el hilo termine
         print("Hilo de procesamiento detenido.")
+        if puertoSerial_c != None:
+            puertoSerial_c.write(mensaje.encode('utf-8'))
     if puertoSerial_c != None:
-        puertoSerial_c.write(mensaje.encode('utf-8'))
-        cerrar_puerto(puertoSerial_c, "C") # Cerrar el puerto serial
+        cerrar_puerto(puertoSerial_c, "Receptor Encoder") # Cerrar el puerto serial
         puertoSerial_c = None # importante setear a none para que se pueda volver a abrir
 
 
@@ -133,7 +131,7 @@ def conexion():
     global Estado, envio_encoder, Estado_reset, port_lista, selec_imu, puertoSerial_c, envio_encoder_listo, Ticks, ti_ant, constante, data_thread
     mensaje_error.config(text="")
     Estado = 1
-
+    
     if not port_lista.get() or not odometro_lista.get():
         mensaje_error.config(text="Falta seleccionar el puerto o el tipo de odómetro.")
         Estado = 0
@@ -189,46 +187,37 @@ def conexion():
                 # Si ocurre un error al intentar convertir a float, muestra un mensaje de error
                 mensaje_error.config(text="Error: El valor ingresado no es válido. Ingrese un número válido. Ejemplo: 0.05. Use punto para los decimales")
 
-
-        if envio_encoder:
-            if envio_encoder_listo: 
-                mensaje = f"RUN"
-                #print(mensaje)
-                puertoSerial_c.write(mensaje.encode('utf-8'))
-                envio_encoder = False
-            else:# solo envía encoder una vez. Si se quiere cambiar de encoder se debe cerrar la aplicación
-                mensaje = f"RUN,{encoder_type},{encoder_ratio}"
-                #print(mensaje)
-                puertoSerial_c.write(mensaje.encode('utf-8'))
-                envio_encoder = False
-                envio_encoder_listo = True
+        texto_error = mensaje_error.cget("text")
+        if texto_error != "" and texto_error != "Advertencia: Queda poca batería":
+            mensaje_error2.config(text="Error: Configuración errónea. Vuelva a conectar")
+            desconectar()
+        else:
+            mensaje_error.config(text="")
+            if envio_encoder:
+                if envio_encoder_listo: 
+                    mensaje = f"RUN"
+                    #print(mensaje)
+                    puertoSerial_c.write(mensaje.encode('utf-8'))
+                    envio_encoder = False
+                else:# solo envía encoder una vez. Si se quiere cambiar de encoder se debe cerrar la aplicación
+                    mensaje = f"RUN,{encoder_type},{encoder_ratio}"
+                    #print(mensaje)
+                    puertoSerial_c.write(mensaje.encode('utf-8'))
+                    envio_encoder = False
+                    envio_encoder_listo = True
+                
+                    # Iniciar el bucle de procesamiento en un hilo separado
+                data_thread = threading.Thread(target=data_processing_loop)
+                data_thread.daemon = True #Para que el hilo se cierre cuando se cierra la ventana
+                data_thread.start()
+                update_gui()
     
     
     else:
 
         Estado = 0
+         
 
-
-            
-
-
-    # Iniciar el bucle de procesamiento en un hilo separado
-    data_thread = threading.Thread(target=data_processing_loop)
-    data_thread.daemon = True #Para que el hilo se cierre cuando se cierra la ventana
-    data_thread.start()
-    update_gui()
-
-   # gui_thread = threading.Thread(target=update_gui)
-   # gui_thread.daemon = True
-   # gui_thread.start()
-
-  #  if selec_imu == 1:
-   #     puertoSerial.flushInput()
-    #    puertoSerial.close()
-     #   puertoSerial_b.close()
-    #if port_lista.get() and odometro_lista.get():
-     #   puertoSerial_c.flushInput()
-      #  puertoSerial_c.close()
 
 def data_processing_loop():
     global Estado, Ticks, ti_ant, Estado_reset, constante, data_queue, Distancia_str, Bat_str, data2_queue, reset_activado
@@ -237,11 +226,14 @@ def data_processing_loop():
     tiempos_de_ciclo = []
     data_queue = queue.Queue()
     data2_queue = queue.Queue()
-    distancia_anterior = None
+    distancia_anterior_str = None
     bat_int_anterior = None
     ticks_anteriores = None
     pitch = "+00.0"
     roll = "+000.0"
+    
+    mensaje_error.config(text="")
+    mensaje_error2.config(text="")
     nombre_archivo = os.path.join(current_folder, "CSVs de prueba", f"{datetime.datetime.now().strftime('%H.%M.%S.%f')}.csv")
 
     with open(nombre_archivo, "w", newline="") as file:
@@ -290,9 +282,9 @@ def data_processing_loop():
                         Ticks -= diferencial
 
                         Distancia = round(Ticks * constante, 2)
-                        if Distancia != distancia_anterior:
-                            Distancia_str = f"{Distancia:+08.2f}"
-                            distancia_anterior = Distancia #Actualizar la distancia anterior
+                        Distancia_str = f"{Distancia:+08.2f}"
+                        if Distancia_str != distancia_anterior_str:
+                            distancia_anterior_str = Distancia_str #Actualizar la distancia anterior
                             ticks_anteriores = Ti_int #Actualizar los ticks anteriores
                             data_queue.put(Distancia_str) 
                         if Bat_int <= 10:
@@ -306,8 +298,12 @@ def data_processing_loop():
                             data2_queue.put(Bat_str)
                     
                     else:
-                        Distancia_str = f"{distancia_anterior:+08.2f}"
-                        Bat_str = f"{bat_int_anterior}"
+                        if Distancia_str != distancia_anterior_str:
+                            distancia_anterior_str = Distancia_str #Actualizar la distancia anterior
+                            data_queue.put(Distancia_str)
+                        else:
+                            Distancia_str = distancia_anterior_str
+                            Bat_str = f"{bat_int_anterior}"
                     
                         
 
@@ -318,7 +314,7 @@ def data_processing_loop():
             break
         
         tiempo_actual = time.perf_counter()
-        if tiempo_actual - tiempo_anterior_escritura >= 1/31:
+        if tiempo_actual - tiempo_anterior_escritura >= 1/30:
             with open(nombre_archivo, 'a', newline='') as archivo:
                 escritor = csv.writer(archivo, delimiter=';', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
                 hora_actual_unix = datetime.datetime.now().timestamp()
@@ -333,16 +329,6 @@ def data_processing_loop():
         elapsed_time = time.perf_counter() - start_time
         tiempos_de_ciclo.append(elapsed_time)
         target_time = 1/31
-
-       # if elapsed_time > target_time:
-        #    pass
-          #  print(f"No se cumple el tiempo objetivo. Tiempo transcurrido: {elapsed_time:.6f}") #Imprime tiempo transcurrido
-
-        if tiempos_de_ciclo:
-            promedio = sum(tiempos_de_ciclo) / len(tiempos_de_ciclo)
-            maximo = max(tiempos_de_ciclo)
-            minimo = min(tiempos_de_ciclo)
-          ##  print(f"Tiempos de ciclo: Promedio: {promedio:.6f}, Maximo: {maximo:.6f}, Minimo: {minimo:.6f}")
 
 
 def update_gui():
@@ -413,7 +399,7 @@ def habilitar_radio(*args):
 # Configuración de la interfaz gráfica
 ventana = tk.Tk()
 ventana.title("Odometria - Maquintel")
-ventana.geometry('800x350')
+ventana.geometry('550x300')
 ventana.configure(background='dark orange')
 
 logo = tk.PhotoImage(file=logo_path)
@@ -431,9 +417,9 @@ etiqueta3.place(x=80, y=159)
 
 bat = tk.StringVar(value="00")
 etiqueta6 = tk.Label(ventana, text='Batería: ', bg='white', fg='black')
-etiqueta6.place(x=5, y=259)
+etiqueta6.place(x=5, y=190)
 etiqueta7 = tk.Label(ventana, textvariable=bat, bg='white', fg='black', width=9)
-etiqueta7.place(x=80, y=259)
+etiqueta7.place(x=80, y=190)
 
 port_lista = ttk.Combobox(ventana, width=10, values=puertos_seriales())
 port_lista.place(x=60, y=80)
@@ -472,8 +458,11 @@ selec_imu = tk.IntVar()
 check = tk.Checkbutton(ventana, variable=selec_imu, onvalue=1, offvalue=0, bg="dark orange", activebackground="dark orange", text='IMU')
 check.place(x=5, y=115)
 
-mensaje_error = tk.Label(ventana, text="", bg="white", fg="red", font=("Helvetica", 10))
-mensaje_error.place(x=5, y=190)
+mensaje_error = tk.Label(ventana, text="Rellene los parámetros de configuración", bg="white", fg="red", font=("Helvetica", 10))
+mensaje_error.place(x=5, y=229)
+
+mensaje_error2 = tk.Label(ventana, text="", bg="white", fg="red", font=("Helvetica", 10))
+mensaje_error2.place(x=5, y=256)
 
 odometro_lista.bind("<<ComboboxSelected>>", habilitar_radio)
 
